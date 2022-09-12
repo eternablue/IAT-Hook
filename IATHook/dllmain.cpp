9089e113a5fff6e60e4ee37303b4a07ea2ef3d49
eternablue <91263767+eternablue@users.noreply.github.com>
2022-09-12 19:17:03 +0200

    Add files via upload

---
+++ IATHook/dllmain.cpp
@@ -0,0 +1,52 @@
+#include <Windows.h>
+#include <stdint.h>
+#include <stdio.h>
+
+void hookfunction()
+{
+	MessageBoxA(0, "Hello from hook !", "Hooked", 0);
+}
+
+void HookIAT()
+{
+	uint64_t process_base = (uint64_t)GetModuleHandleA(0);
+	uint64_t routine = (uint64_t)GetProcAddress(GetModuleHandleA("USER32.dll"), "TranslateMessage");
+	
+	PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)process_base;
+	PIMAGE_NT_HEADERS nt_header = (PIMAGE_NT_HEADERS)(process_base + dos_header->e_lfanew);
+	PIMAGE_IMPORT_DESCRIPTOR import_descriptor = (PIMAGE_IMPORT_DESCRIPTOR)(process_base + nt_header->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+	
+	while (import_descriptor->Name)
+	{
+		PIMAGE_THUNK_DATA original_first_thunk = (PIMAGE_THUNK_DATA)(process_base + import_descriptor->OriginalFirstThunk);
+		PIMAGE_THUNK_DATA firstThunk = (PIMAGE_THUNK_DATA)(process_base + import_descriptor->FirstThunk);
+		
+		while (original_first_thunk->u1.AddressOfData)
+		{
+			PIMAGE_IMPORT_BY_NAME functionName = (PIMAGE_IMPORT_BY_NAME)(process_base + original_first_thunk->u1.AddressOfData );
+
+			if (firstThunk->u1.Function == routine)
+			{
+				DWORD old_protection, changed_protection;
+				VirtualProtect(&firstThunk->u1.Function, sizeof(uint64_t), PAGE_READWRITE, &old_protection);
+				firstThunk->u1.Function = (uint64_t)hookfunction;
+				VirtualProtect(&firstThunk->u1.Function, sizeof(uint64_t), old_protection, &changed_protection);
+
+				return;
+			}
+			original_first_thunk++;
+			firstThunk++;
+		}
+		import_descriptor++;
+	}
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
+		HookIAT();
+        return 1;
+    }
+    return 1;
+}
